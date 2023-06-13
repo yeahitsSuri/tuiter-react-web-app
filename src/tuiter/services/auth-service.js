d4ee72e505ddc96ca3f01366049bde9ed0970d79
Tianshu Yang <yts9614@gmail.com>
2023-06-14 02:33:14 +0800

    upd auth-service

--- src/tuiter/services/auth-service.js
+++ src/tuiter/services/auth-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const SERVER_API_URL = process.env.REACT_APP_SERVER_API_URL;
-const USERS_URL = `${SERVER_API_URL}/users`;
+const USERS_URL = `${SERVER_API_URL}/api/users`;
 
 
 const api = axios.create({ withCredentials: true });
