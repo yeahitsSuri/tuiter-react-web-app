622982992ebcecc736bb8ead600a4f850afb773f
Tianshu Yang <yts9614@gmail.com>
2023-06-20 20:42:36 +0800

    change code format

--- src/tuiter/services/auth-thunks.js
+++ src/tuiter/services/auth-thunks.js
@@ -25,7 +25,7 @@ export const updateUserThunk = createAsyncThunk(
       await authService.updateUser(userId, user);
       return user;
     }
-  );
+);
 
 
 export const registerThunk = createAsyncThunk(
